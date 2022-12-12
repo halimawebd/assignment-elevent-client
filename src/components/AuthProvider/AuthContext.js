384bad27d9262c68e8e19324ac30a2650dd653d4
halimawebd <halimawebd@gmail.com>
2022-12-12 22:47:53 +0600

    router part

--- src/components/AuthProvider/AuthContext.js
+++ src/components/AuthProvider/AuthContext.js
@@ -1,67 +1,67 @@
-// import React, { createContext, useEffect, useState } from "react";
-// import { app } from "../../firebase";
-// import {
-//   createUserWithEmailAndPassword,
-//   getAuth,
-//   GoogleAuthProvider,
-//   onAuthStateChanged,
-//   signInWithEmailAndPassword,
-//   signInWithPopup,
-//   signOut,
-//   updateProfile,
-// } from "firebase/auth";
-// const auth = getAuth(app);
-// export const userContext = createContext();
-// const googleProvider = new GoogleAuthProvider();
-// const AuthContext = ({ children }) => {
-//   const [user, setUser] = useState("");
-//   const [loading, setLoading] = useState(true);
+import React, { createContext, useEffect, useState } from "react";
+import { app } from "../../firebase";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+const auth = getAuth(app);
+export const userContext = createContext();
+const googleProvider = new GoogleAuthProvider();
+const AuthContext = ({ children }) => {
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
 
-//   const userRegister = (email, password) => {
-//     setLoading(true);
-//     return createUserWithEmailAndPassword(auth, email, password);
-//   };
+  const userRegister = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-//       setUser(currentUser);
-//       setLoading(false);
-//     });
-//     return () => {
-//       unsubscribe();
-//     };
-//   }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-//   const logIn = (email, password) => {
-//     setLoading(true);
-//     return signInWithEmailAndPassword(auth, email, password);
-//   };
+  const logIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-//   //   const updateUser = (profile)=>{
-//   //     return updateProfile(auth.currentUser, profile)
-//   //   }
+  //   const updateUser = (profile)=>{
+  //     return updateProfile(auth.currentUser, profile)
+  //   }
 
-//   const logOut = () => {
-//     setLoading(true);
-//     return signOut(auth);
-//   };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-//   const logInWithGoogle = () => {
-//     setLoading(true)
-//     return signInWithPopup(auth, googleProvider);
-//   };
+  const logInWithGoogle = () => {
+    setLoading(true)
+    return signInWithPopup(auth, googleProvider);
+  };
 
-//   const userInfo = {
-//     user,
-//     userRegister,
-//     logIn,
-//     logInWithGoogle,
-//     logOut,
-//     loading,
-//   };
-//   return (
-//     <userContext.Provider value={userInfo}>{children}</userContext.Provider>
-//   );
-// };
+  const userInfo = {
+    user,
+    userRegister,
+    logIn,
+    logInWithGoogle,
+    logOut,
+    loading,
+  };
+  return (
+    <userContext.Provider value={userInfo}>{children}</userContext.Provider>
+  );
+};
 
-// export default AuthContext;
+export default AuthContext;
--- src/components/Routes/Router.js
+++ src/components/Routes/Router.js
@@ -1,102 +1,102 @@
-// import { createBrowserRouter } from "react-router-dom";
-// import Main from "../Layouts/Main/Main";
-// import About from "../pages/About/About";
-// import AddReview from "../pages/AddReview/AddReview";
-// import AddService from "../pages/AddService/AddService";
-// import AllService from "../pages/AllService/AllService";
-// import Blog from "../pages/Blog/Blog";
-// import Contact from "../pages/Contact/Contact";
-// import ErrorPage from "../pages/ErrorPage/ErrorPage";
-// import Home from "../pages/Home/Home";
-// import Login from "../pages/Login/Login";
-// import MyReviews from "../pages/MyReviews/MyReviews";
-// import MyServices from "../pages/MyServices/MyServices";
-// import Register from "../pages/Register/Register";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main/Main";
+import About from "../pages/About/About";
+import AddReview from "../pages/AddReview/AddReview";
+import AddService from "../pages/AddService/AddService";
+import AllService from "../pages/AllService/AllService";
+import Blog from "../pages/Blog/Blog";
+import Contact from "../pages/Contact/Contact";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import MyReviews from "../pages/MyReviews/MyReviews";
+import MyServices from "../pages/MyServices/MyServices";
+import Register from "../pages/Register/Register";
 
-// import ServicesDetails from "../pages/ServicesDetails/ServicesDetails";
-// import Update from "../pages/Update/Update";
-// import UserReview from "../pages/UserReview/UserReview";
-// import PrivateRoutes from "../PrivateRoutes/PrivateRoutes";
+import ServicesDetails from "../pages/ServicesDetails/ServicesDetails";
+import Update from "../pages/Update/Update";
+import UserReview from "../pages/UserReview/UserReview";
+import PrivateRoutes from "../PrivateRoutes/PrivateRoutes";
 
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     errorElement: <ErrorPage />,
-//     element: <Main />,
-//     children: [
-//       {
-//         path: "/",
-//         loader: () => fetch("https://assignment-11-server-iota.vercel.app/service"),
-//         element: <Home />,
-//       },
-//       {
-//         path: "/home",
-//         loader: () => fetch("https://assignment-11-server-iota.vercel.app/service"),
-//         element: <Home />,
-//       },
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    errorElement: <ErrorPage />,
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        loader: () => fetch("https://assignment-11-server-iota.vercel.app/service"),
+        element: <Home />,
+      },
+      {
+        path: "/home",
+        loader: () => fetch("https://assignment-11-server-iota.vercel.app/service"),
+        element: <Home />,
+      },
 
-//       {
-//         path: "/login",
-//         element: <Login />,
-//       },
-//       {
-//         path: "/register",
-//         element: <Register />,
-//       },
-//       {
-//         path: "/myServices",
-//         loader: () => fetch("https://assignment-11-server-iota.vercel.app/allService"),
-//         element: <MyServices />,
-//       },
-//       {
-//         path: "/review/:id",
-//         loader: ({ params }) =>
-//           fetch(`https://assignment-11-server-iota.vercel.app/allService/${params.id}`),
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/myServices",
+        loader: () => fetch("https://assignment-11-server-iota.vercel.app/allService"),
+        element: <MyServices />,
+      },
+      {
+        path: "/review/:id",
+        loader: ({ params }) =>
+          fetch(`https://assignment-11-server-iota.vercel.app/allService/${params.id}`),
 
-//         element: (
-//           <PrivateRoutes>
-//             <AddReview />
-//           </PrivateRoutes>
-//         ),
-//       },
+        element: (
+          <PrivateRoutes>
+            <AddReview />
+          </PrivateRoutes>
+        ),
+      },
 
-//       {
-//         path: "/services/:id",
-//         loader: ({ params }) =>
-//           fetch(`https://assignment-11-server-iota.vercel.app/allService/${params.id}`),
-//         element: <ServicesDetails />,
-//       },
+      {
+        path: "/services/:id",
+        loader: ({ params }) =>
+          fetch(`https://assignment-11-server-iota.vercel.app/allService/${params.id}`),
+        element: <ServicesDetails />,
+      },
 
-//       {
-//         path: "/myReview",
-//         loader: () => fetch("https://assignment-11-server-iota.vercel.app/feedback"),
-//         element: (
-//           <PrivateRoutes>
-//             <MyReviews />
-//           </PrivateRoutes>
-//         ),
-//       },
+      {
+        path: "/myReview",
+        loader: () => fetch("https://assignment-11-server-iota.vercel.app/feedback"),
+        element: (
+          <PrivateRoutes>
+            <MyReviews />
+          </PrivateRoutes>
+        ),
+      },
 
-//       {
-//         path: "/addService",
-//         element: <AddService />,
-//       },
-//       {
-//         path: "/allService",
-//         loader: () => fetch("https://assignment-11-server-iota.vercel.app/allService"),
-//         element: <AllService />,
-//       },
-//       {
-//         path: "/blog",
-//         element: <Blog />,
-//       },
+      {
+        path: "/addService",
+        element: <AddService />,
+      },
+      {
+        path: "/allService",
+        loader: () => fetch("https://assignment-11-server-iota.vercel.app/allService"),
+        element: <AllService />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
      
-//       {
-//         path: "/about", element: <About/>
-//       },
-//       {
-//         path: "/contact", element: <Contact/>
-//       }
-//     ],
-//   },
-// ]);
+      {
+        path: "/about", element: <About/>
+      },
+      {
+        path: "/contact", element: <Contact/>
+      }
+    ],
+  },
+]);
